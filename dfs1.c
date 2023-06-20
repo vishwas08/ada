4eaaab8bb5a91aa2890386f8ef212b958b1e799e
vishwas08 <117823743+vishwas08@users.noreply.github.com>
2023-06-20 15:54:05 +0530

    Add files via upload

--- dfs1.c
+++ dfs1.c
@@ -1,56 +1,39 @@
 #include<stdio.h>
-void DFS(int);
-int a[10][10],vis[10],n;
+void dfs(int);
+int a[10][10], vis[10], n;
 void main()
 {
-
     int i,j;
-    printf("\nEnter no of vertices");
-    scanf("%d",&n);
-    printf("\nEnter adjacent matrix");
+    printf("Enter the vertices\n");
+    scanf("%d", &n);
+    printf("Enter the adjacency matrix\n");
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n;i++)
+        for(j=1;j<=n;j++)
         {
-
-            scanf("%d",&a[i][j]);
+            scanf("%d", &a[i][j]);
         }
     }
     for(i=1;i<=n;i++)
+    {
         vis[i]=0;
-    printf("\nEnter DFS traversal");
+    }
+    printf("dfs traversal");
     for(i=1;i<=n;i++)
     {
         if(vis[i]==0)
-        DFS(i);
+            dfs(i);
     }
-    connect();
-    getch();        //DFS(i);
-}
-void connect()
-{
-
-    for(int i=1;i<=n;i++)
-    {
-        if(vis[i]==0)
-        {
-            printf("\nNot Connected\n");
-            return;
-        }
-    }
-    printf("\nConnected");
 }
 
-
-void DFS(int v)
+void dfs(int v)
 {
-
     int i;
     vis[v]=1;
-    printf("\n%d",v);
+    printf("%d",v);
     for(i=1;i<=n;i++)
     {
-        if(a[v][i]==1&&vis[i]==0)
-            DFS(i);
+        if(a[v][i]==1 && vis[i]==0)
+            dfs(i);
     }
 }
